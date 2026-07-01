6406332eab8725736ad44bc884502dc01de07e02
Arthur Salerno <70231798+Tuzapeno@users.noreply.github.com>
2026-06-30 22:46:50 -0300

    feat: white

--- src/TerminalUI.py
+++ src/TerminalUI.py
@@ -54,7 +54,7 @@ class TerminalUI:
             curses.start_color()
             curses.use_default_colors()
             curses.init_pair(1, curses.COLOR_WHITE, -1)   # Standard
-            curses.init_pair(2, curses.COLOR_CYAN, -1)    # Headers/Cyan
+            curses.init_pair(2, curses.COLOR_WHITE, -1)    # Headers/Cyan
             curses.init_pair(3, curses.COLOR_GREEN, -1)   # Completed
             curses.init_pair(4, curses.COLOR_YELLOW, -1)  # Focus / Highlight
             curses.init_pair(5, curses.COLOR_RED, -1)     # Accents/Incomplete
